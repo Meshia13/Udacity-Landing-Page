d643a2b0ca09723cf0588440854aa153c913bc0c
Carmeshia <65563803+Meshia13@users.noreply.github.com>
2024-06-03 22:21:08 -0400

    docs: added href attributes to navbar

--- js/app.js
+++ js/app.js
@@ -26,8 +26,17 @@
 // Getting the <ul> element and assigning to variable
 const myNav = document.getElementById("navbar__list");
 
- // Selecting all sections
- const sections = document.querySelectorAll("section");
+// Selecting all sections
+const sections = document.querySelectorAll("section");
+
+ 
+// Array of nav items, or <li> elements, to append to the <ul> element
+const navList = [
+    "Section 1", 
+    "Section 2",
+    "Section 3", 
+    "Section 4" 
+];
 
 
 /**
@@ -35,6 +44,16 @@ const myNav = document.getElementById("navbar__list");
  * Start Helper Functions
  * 
 */
+function getAnchorId() {
+    const getId = document.querySelectorAll("[id]")
+
+    for (let i =1; i < getId.length; i++){
+        const myIds = getId[i].id;
+        return myIds;
+    //    console.log(myIds)
+    }
+    
+}
 
 
 
@@ -45,53 +64,48 @@ const myNav = document.getElementById("navbar__list");
 */
 
 // build the nav
-
-// Array of nav items, or <li> elements, to append to the <ul> element
-const navList = [
-    "Section 1", 
-    "Section 2",
-    "Section 3", 
-    "Section 4" 
-];
-
-// Looping through navList and appending it to <ul> element
-for (let i =0; i < navList.length; i++) {
-    let li = document.createElement("li");
-    // Appending <li> to parent element <ul>
-    myNav.append(li);
-    // adding a class attribute to the <li> elements
-    li.classList.add("menu__link");
-    // adding id atribute
-    li.setAttribute("href", `#section${i+1}`);
-    li.innerHTML = navList[i];
-    myNav.appendChild(li);
-    
+function buildNav () {
+    // Looping through navList and appending it to <ul> element
+    for (let i =0; i < navList.length; i++) {
+        // creating <li> element
+        let li = document.createElement("li");
+        // Appending <li> to parent element <ul>
+        myNav.append(li);
+        // adding a class attribute to the <li> elements
+        li.classList.add("menu__link");
+        // creating <a> element and links to anchor with section id
+        let a = document.createElement("a");
+        a.setAttribute("href", `#section${i+1}`);
+        // a.setAttribute("href", `#${getAnchorId()}`);
+        a.innerHTML = navList[i];
+        li.appendChild(a);
+        myNav.appendChild(li);
+        
+    }
 }
 
-
 // Add class 'active' to section when near top of viewport
-
-// scroll
-document.addEventListener("scroll", function(){
+function makeActive() {
+    document.addEventListener("scroll", function(){
     
-    function allSections (section){
-
-        const topPage = section.getBoundingClientRect().top;
-
-        // class is active if it is between 0 - 100px
-        if (topPage > 0 && topPage < 100) {
-            section.classList.add("your-active-class");
+        function allSections (section){
+    
+            const topPage = section.getBoundingClientRect().top;
+    
+            // class is active if it is between 0 - 100px
+            if (topPage > 0 && topPage < 100) {
+                section.classList.add("your-active-class");
+            }
+            else {
+                // if not, class is removed
+                section.classList.remove("your-active-class");
+            }
         }
-        else {
-            // if not, class is removed
-            section.classList.remove("your-active-class");
-        }
-    }
-
-    // looping through each section
-    sections.forEach(allSections);
-})
-
+    
+        // looping through each section
+        sections.forEach(allSections);
+    })
+}
 
 // Scroll to anchor ID using scrollTO event
 
@@ -102,9 +116,11 @@ document.addEventListener("scroll", function(){
 */
 
 // Build menu 
+buildNav ();
 
 // Scroll to section on link click
 
 // Set sections as active
+makeActive();
 
 
